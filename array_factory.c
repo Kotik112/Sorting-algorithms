1e708e68804518c1bdccd32d33ac0dae3f177972
Kotik <88910492+Kotik112@users.noreply.github.com>
2021-12-13 21:04:01 +0100

    array factory almost complete

--- array_factory.c
+++ array_factory.c
@@ -4,10 +4,29 @@
 #include "heapsort.h"
 #include "array_factory.h"
 
-int *create_array(int size_n) {
+
+int * create_random_array(int size_n) {
     int * array = malloc(sizeof(int) * size_n);
     for(int i = 0; i < size_n; i++)
        array[i]=rand() % 100;
     return array;
 }
 
+int * create_sorted_array(int size_n) {
+    int * array = create_array(size_n);
+    heap_sort(array, size_n);
+    return array;
+}
+
+int * create_reverse_sorted_array(int size_n) {
+    int * array = create_array(size_n);
+    //bubble sort in reverse here?
+    return array;
+}
+
+int * create_almost_sorted_array(int size_n) {
+    int * array = create_array(size_n);
+    int length = (int) (size_n * 0.9);  //funkar detta? 90% av size_n
+    heap_sort(array, length);
+    return array;
+}
