0e4d9c7f8ce2df188ec3fd3dfa3d95d5c8043f34
UnnGledje <unn.gledje@gmail.com>
2021-12-14 14:57:52 +0100

    Added function for creating char array

--- selection_sort.c
+++ selection_sort.c
@@ -8,14 +8,39 @@ void swap(int *x_ptr,int *y_ptr) {
     *y_ptr = tmp;
 }
 
-/*
+
 static int *create_array(int size_n) {
   int *array = malloc(sizeof(int) * size_n);
   for(int i = 0; i < size_n; i++)
      array[i]=rand() % 1000;
   return array;
 }
-*/
+
+
+static char *create_array_char(int size_n) {
+    char *array = malloc(sizeof(char) * size_n);
+    for(int i = 0; i < size_n; i++)
+        array[i] = 'A' + rand() % 97;
+    return array;
+}
+
+void sort_char(char arr[], int n) {
+    int i, j, idx;
+
+    for(int i = 0; i < n; i++){
+        idx = i;
+        for(int j = i; j < n; j++){
+            if(arr[j] < arr[idx]) {
+                idx = j;
+               
+            }
+            
+        }
+      
+        swap_char(&arr[idx],&arr[i]);
+    }
+    
+}
 
 
 void sort(int arr[], int n) {
@@ -39,6 +64,13 @@ void sort(int arr[], int n) {
 void print(int arr[], int size) {
     for(int i = 0; i < size; i++) {
         printf("Element %d :: %d",i, arr[i]);
+        printf("\n");
+    }
+}
+
+void print_char(char arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        printf("Element %d :: %c",i, arr[i]);
         printf("\n");
     }
 }
--- selection_sort.h
+++ selection_sort.h
@@ -4,9 +4,13 @@
 
 
 void swap(int *x_ptr,int *y_ptr);
-//static int *create_array(int size_n);
+void swap_char(char *x_ptr,char *y_ptr);
+static int *create_array(int size_n);
+static char *create_array_char(int size_n);
 void sort(int arr[], int n);
+void sort_char(char arr[], int n);
 void print(int arr[], int size);
+void print_char(char arr[], int size);
 
 
 
