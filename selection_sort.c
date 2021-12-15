4c2f3df789d4e7f2199e3082411c1be6d7190878
UnnGledje <unn.gledje@gmail.com>
2021-12-16 00:36:02 +0100

    cv

--- selection_sort.c
+++ selection_sort.c
@@ -11,9 +11,9 @@ static void selection_sort_swap_int(int *x_ptr,int *y_ptr) {
 void selection_sort_int(int arr[], int n) {
     int i, j, idx;
 
-    for(int i = 0; i < n; i++){
+    for(i = 0; i < n; i++){
         idx = i;
-        for(int j = i; j < n; j++){
+        for(j = i; j < n; j++){
             if(arr[j] < arr[idx]) {
                 idx = j;
             }
