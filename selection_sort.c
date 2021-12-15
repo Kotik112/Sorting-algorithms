2a569044c703313d0818542b460d84d5d3227233
UnnGledje <unn.gledje@gmail.com>
2021-12-15 13:54:58 +0100

    removed functions for char

--- selection_sort.c
+++ selection_sort.c
@@ -2,30 +2,10 @@
 #include <time.h>
 #include <stdlib.h>
 
-void selection_sort_swap_int(int *x_ptr,int *y_ptr) {
+static void selection_sort_swap_int(int *x_ptr,int *y_ptr) {
     int tmp = *x_ptr;
     *x_ptr = *y_ptr;
     *y_ptr = tmp;
-}
-
-void selection_sort_swap_char(char *x_ptr,char *y_ptr) {
-    char tmp = *x_ptr;
-    *x_ptr = *y_ptr;
-    *y_ptr = tmp;
-} 
-
-void selection_sort_char(char arr[], int n) {
-    int i, j, idx;
-
-    for(int i = 0; i < n; i++){
-        idx = i;
-        for(int j = i; j < n; j++){
-            if(arr[j] < arr[idx]) {
-                idx = j;
-            }
-        }
-        selection_sort_swap_char(&arr[idx],&arr[i]);
-    } 
 }
 
 void selection_sort_int(int arr[], int n) {
@@ -49,11 +29,6 @@ void selection_sort_print_int(int arr[], int size) {
     }
 }
 
-void selection_sort_print_char(char arr[], int size) {
-    for(int i = 0; i < size; i++) {
-        printf("Element %d :: %c",i, arr[i]);
-        printf("\n");
-    }
-}
+
 
 
--- selection_sort.h
+++ selection_sort.h
@@ -1,15 +1,16 @@
 #ifndef _SELECTION_SORT_H_
 #define _SELECTION_SORT_H_
-#endif
 
 
-void selection_sort_swap_int(int *x_ptr,int *y_ptr);
-void selection_sort_swap_char(char *x_ptr,char *y_ptr);
+
+
+/* Function to swap and sort elements in ascending order */
 void selection_sort_int(int arr[], int n);
-void selection_sort_char(char arr[], int n);
+
+/* Function to print selection sort */
 void selection_sort_print_int(int arr[], int size);
-void selection_sort_print_char(char arr[], int size);
 
 
+#endif
 
 
