c2f8e0154a90241944c7de48b9e238b50724f878
UnnGledje <unn.gledje@gmail.com>
2021-12-15 13:13:02 +0100

    name changes of functions

--- selection_sort.c
+++ selection_sort.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-void selection_sort_swap(int *x_ptr,int *y_ptr) {
+void selection_sort_swap_int(int *x_ptr,int *y_ptr) {
     int tmp = *x_ptr;
     *x_ptr = *y_ptr;
     *y_ptr = tmp;
@@ -42,7 +42,7 @@ void selection_sort_int(int arr[], int n) {
     }
 }
 
-void selection_sort_print(int arr[], int size) {
+void selection_sort_print_int(int arr[], int size) {
     for(int i = 0; i < size; i++) {
         printf("Element %d :: %d",i, arr[i]);
         printf("\n");
--- selection_sort.h
+++ selection_sort.h
@@ -3,11 +3,11 @@
 #endif
 
 
-void selection_sort_swap(int *x_ptr,int *y_ptr);
+void selection_sort_swap_int(int *x_ptr,int *y_ptr);
 void selection_sort_swap_char(char *x_ptr,char *y_ptr);
-void selection_sort(int arr[], int n);
+void selection_sort_int(int arr[], int n);
 void selection_sort_char(char arr[], int n);
-void selection_sort_print(int arr[], int size);
+void selection_sort_print_int(int arr[], int size);
 void selection_sort_print_char(char arr[], int size);
 
 
